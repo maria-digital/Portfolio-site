dd9585c297735959ff503ad91ff3b54ec19c08c7
maria-land <57145395+maria-land@users.noreply.github.com>
2019-12-10 17:10:00 +0000

    Add script

---
+++ script.js
@@ -0,0 +1,18 @@
+var x = prompt("Введите число 1", 23);
+
+console.log(x);
+
+var y = prompt("Введите число 2", 50);
+
+console.log(y);
+
+x = parseInt("23", 10);
+
+y = parseInt("50", 10);
+
+
+var sum = x+y;
+
+alert(sum)
+
+console.log(sum);
